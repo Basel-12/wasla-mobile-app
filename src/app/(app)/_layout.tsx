d893518e254d87970e7c9a6ee06e044aecf23622
Basel-12 <bassilfathy100@gmail.com>
2026-04-22 11:19:49 +0200

    init app icon change

---
+++ src/app/(app)/_layout.tsx
@@ -0,0 +1,14 @@
+import FloatingTabBar from "@/components/FloatingTabBar";
+import { Tabs } from "expo-router";
+
+export default function AppLayout() {
+	return (
+		<Tabs tabBar={(props) => <FloatingTabBar {...props} />}>
+			<Tabs.Screen name="(home)/index" options={{ title: "Home", headerShown: false }} />
+			<Tabs.Screen
+				name="(profile)/index"
+				options={{ title: "Profile" }}
+			/>
+		</Tabs>
+	);
+}
